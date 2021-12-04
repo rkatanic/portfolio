cd42215a2388adca92872cbe83759801af5ff0cf
Radivoje Katanic <k.rade313@gmail.com>
2021-12-04 20:04:32 +0100

    Redesign about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,48 @@ import "./About.css";
 
 const About = (): JSX.Element => (
   <Section id="about">
-    <h3>about</h3>
+    <div className="about">
+      <div className="about-item">
+        <h3 className="about-item-title">Experience</h3>
+        <div className="about-item-content">
+          <p className="about-item-content-date">08/2019 - present</p>
+          <div>
+            <p className="about-item-content-headline">Software Developer</p>
+            <p className="about-item-content-description">ProductDock</p>
+          </div>
+        </div>
+        <div className="about-item-content">
+          <p className="about-item-content-date">03/2019 - 06/2019</p>
+          <div>
+            <p className="about-item-content-headline">Intern</p>
+            <p className="about-item-content-description">
+              ProductDock Rookie Boot Camp
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="about-item">
+        <h3 className="about-item-title">Education</h3>
+        <div className="about-item-content">
+          <p className="about-item-content-date">2015 - 2019</p>
+          <div>
+            <p className="about-item-content-headline">IT Engineer</p>
+            <p className="about-item-content-description">
+              Slobomir P University
+            </p>
+          </div>
+        </div>
+        <div className="about-item-content">
+          <p className="about-item-content-date">2011 - 2015</p>
+          <div>
+            <p className="about-item-content-headline">Computer Technician</p>
+            <p className="about-item-content-description">
+              Electrotehnical High School in Doboj
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </Section>
 );
 
