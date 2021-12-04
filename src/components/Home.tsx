51da1322dc553bf727400c98f59e70c550728be8
Radivoje Katanic <k.rade313@gmail.com>
2021-12-04 19:09:08 +0100

    Add progress bar indicator

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useIsDarkMode, useToggleDarkMode } from "../context/GlobalContext";
 import Typewriter from "typewriter-effect";
 import "./Home.css";
@@ -29,9 +29,29 @@ const Home = (): JSX.Element => {
     shouldShowDarkMode();
   }, [isDarkMode]);
 
+  const [scroll, setScroll] = useState(0);
+
+  const onScroll = () => {
+    const Scrolled = document.documentElement.scrollTop;
+    const MaxHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const ScrollPercent = (Scrolled / MaxHeight) * 100;
+    setScroll(ScrollPercent);
+  };
+
+  window.addEventListener("scroll", onScroll);
+
   return (
     <div className="container">
       <div className="column-left">
+        <div className="progress-bar">
+          <div className="progress-bar-pulse"></div>
+          <div
+            className="progress-bar-percent"
+            style={{ height: `${scroll}%` }}
+          ></div>
+        </div>
         <div className="actions">
           <div className="night-mode-switch" onClick={toggleDarkMode}>
             {isDarkMode ? <SunIcon /> : <MoonIcon />}
