1b573b44bc8758465c52ac60b09ac41510a44491
Radivoje Katanic <k.rade313@gmail.com>
2021-12-10 23:32:24 +0100

    Animate side nav toggle

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,51 +11,51 @@ const Navigation = (): JSX.Element => {
   const handleMenuToggle = (): void => setShowMenu((prevState) => !prevState);
 
   return (
-    <header className="header">
-      <nav className="nav">
-        <div className={showMenu ? `nav-menu show-menu` : `nav-menu`}>
-          <Scrollspy
-            offset={-30}
-            className="nav-list"
-            currentClassName="active"
-            items={["about", "skills", "faq", "contact"]}
-          >
-            <li className="nav-item" onClick={() => setShowMenu(false)}>
-              <a href="#about" className="nav-link">
-                <div className="number">01</div>
-                <div className="dash"></div>
-                <span>About</span>
-              </a>
-            </li>
-            <li className="nav-item" onClick={() => setShowMenu(false)}>
-              <a href="#skills" className="nav-link">
-                <div className="number">02</div>
-                <div className="dash"></div>
-                <span>Skills</span>
-              </a>
-            </li>
-            <li className="nav-item" onClick={() => setShowMenu(false)}>
-              <a href="#faq" className="nav-link">
-                <div className="number">03</div>
-                <div className="dash"></div>
-                <span>FAQ</span>
-              </a>
-            </li>
-            <li className="nav-item" onClick={() => setShowMenu(false)}>
-              <a href="#contact" className="nav-link">
-                <div className="number">04</div>
-                <div className="dash"></div>
-                <span>Contact</span>
-              </a>
-            </li>
-          </Scrollspy>
-          <CloseIcon className="nav-close" onClick={handleMenuToggle} />
-        </div>
-        <div className="nav-toggle" onClick={handleMenuToggle}>
-          <MenuIcon />
-        </div>
-      </nav>
-    </header>
+    <nav className="nav">
+      <div className={showMenu ? `nav-menu show-menu` : `nav-menu`}>
+        <Scrollspy
+          offset={-30}
+          className="nav-list"
+          currentClassName="active"
+          items={["about", "skills", "faq", "contact"]}
+        >
+          <li className="nav-item" onClick={() => setShowMenu(false)}>
+            <a href="#about" className="nav-link">
+              <div className="number">01</div>
+              <div className="dash"></div>
+              <span>About</span>
+            </a>
+          </li>
+          <li className="nav-item" onClick={() => setShowMenu(false)}>
+            <a href="#skills" className="nav-link">
+              <div className="number">02</div>
+              <div className="dash"></div>
+              <span>Skills</span>
+            </a>
+          </li>
+          <li className="nav-item" onClick={() => setShowMenu(false)}>
+            <a href="#faq" className="nav-link">
+              <div className="number">03</div>
+              <div className="dash"></div>
+              <span>FAQ</span>
+            </a>
+          </li>
+          <li className="nav-item" onClick={() => setShowMenu(false)}>
+            <a href="#contact" className="nav-link">
+              <div className="number">04</div>
+              <div className="dash"></div>
+              <span>Contact</span>
+            </a>
+          </li>
+        </Scrollspy>
+      </div>
+      <div
+        className={`nav-toggle ${showMenu ? "close" : ""}`}
+        onClick={handleMenuToggle}
+      >
+        <div className="nav-toggle-dash"></div>
+      </div>
+    </nav>
   );
 };
 
