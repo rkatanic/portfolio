1c7b39bd3fc304fcee66b72bd55c1f2b4eeacb75
Radivoje Katanic <k.rade313@gmail.com>
2021-12-13 23:24:36 +0100

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 if (localStorage.getItem("dark-mode")) {
-  document.body.setAttribute("class", "day-mode");
+  document.body.classList.add("day-mode");
 }
+
+if (localStorage.getItem("curved-borders")) {
+  document.body.classList.add("curved-borders");
+}
+
 if (!isNaN(parseInt(window.localStorage.getItem("color")?.valueOf()))) {
   window.document.documentElement.style.setProperty(
     "--color-hue",
